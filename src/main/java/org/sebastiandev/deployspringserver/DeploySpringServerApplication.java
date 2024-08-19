1c0bdb8a065814a577e32ae810607cb5462b1a3b
Cauã Sebastian <133614515+Cauasebastian@users.noreply.github.com>
2024-08-18 21:29:48 -0300

    test complete

---
+++ src/main/java/org/sebastiandev/deployspringserver/DeploySpringServerApplication.java
@@ -0,0 +1,13 @@
+package org.sebastiandev.deployspringserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploySpringServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeploySpringServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sebastiandev/deployspringserver/DeploySpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sebastiandev.deployspringserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploySpringServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
